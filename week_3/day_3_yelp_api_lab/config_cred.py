62eb6632e61c89104c3d24b6c55439ec93e7488c
jigme Sherpa <sherpa_jigme@wheatoncollege.edu>
2020-01-26 16:45:59 -0500

    Week_3_changes

---
+++ week_3/day_3_yelp_api_lab/config_cred.py
@@ -0,0 +1,4 @@
+my_Cred = {
+    'client_id' = 'IvYmsjR9a7mY_srfvE8d1w'
+    'api_key' = '-l3rKHolVYw-h0abF8OGqbAHrsoDHWN1DD0IN3d9FfBT7U0I7CPVqLwbQHRgLCuywXYDO7OXxvDe8DZuiqsjEuANbXL8ddVThTtZ-ZQULsvvZ1N4sZAV-3T3CvQpXnYx'
+}
